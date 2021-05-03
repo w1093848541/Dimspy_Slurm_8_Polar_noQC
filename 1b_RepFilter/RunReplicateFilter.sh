f9b4cc78e20b86b09c6f527b646f77b01d346adf
w1093848541 <77172096+w1093848541@users.noreply.github.com>
2021-05-03 13:55:03 -0400

    Update RunReplicateFilter.sh

--- 1b_RepFilter/RunReplicateFilter.sh
+++ 1b_RepFilter/RunReplicateFilter.sh
@@ -28,7 +28,7 @@ dimspy replicate-filter \
 --output RepFilter.$SLURM_JOB_ID \
 --ppm $PPM \
 --replicates $REPLICATES \
---min-peak-present 3 \
+--min-peak-present 6 \
 --report $REPORT_DIR/RepFilterReport.$SLURM_JOB_ID \
 --ncpus $NCPUS
 
